0e259756cb42d397a697741a40f9fa4e1f37d8f3
Giorgos Avramidis <avramidg@gmail.com>
2021-06-10 19:42:25 +0300

    wip

--- lib/bank_data.rb
+++ lib/bank_data.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "bank_data/version"
+require_relative "bank_data/alpha_bank/crawler"
 
 module BankData
   class Error < StandardError; end
---
+++ lib/bank_data/alpha_bank/crawler.rb
@@ -0,0 +1,33 @@
+require 'watir'
+
+module BankData
+  module AlphaBank
+    class Crawler
+      def self.statement_csv(download_path, username, password)
+        prefs = {
+          "download" => {
+            "prompt_for_download" => false,
+            "default_directory" => download_path
+          }
+        }
+
+        browser = Watir::Browser.new :chrome, options: {prefs: prefs}
+
+        browser.goto "https://secure.alpha.gr/Login/myalphaweb/el"
+        browser.text_field(id: "inputUsername").set username
+        browser.text_field(id: "inputPassword").set password
+        browser.button(id: "submitButton").click
+
+        browser.link(text: "Κλείσιμο").click
+        browser.link(text: "Συνέχεια").click
+        browser.goto "https://secure.alpha.gr/myAlphaWeb/business/#/accounts/details/ODgzOTMxNQ==/"
+        sleep 2
+        browser.text_field(id: "dateFrom").set("01/05/2021")
+        browser.text_field(id: "dateTo").set("31/05/2021")
+        browser.link(:"data-ng-click" => "vm.getStatementsWithFilter()").click
+        browser.div(:"data-document-type" => "CSV").links.first.click
+        browser.link(:"data-ng-click" => "logout()").click
+      end
+    end
+  end
+end
