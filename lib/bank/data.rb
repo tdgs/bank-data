71c509bc3eda3156b6feb0d8f9a126bb86a36c64
Giorgos Avramidis <avramidg@gmail.com>
2021-06-10 19:14:50 +0300

    init commit

---
+++ lib/bank/data.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "data/version"
+
+module Bank
+  module Data
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/bank/data_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bank::Data do
+  it "has a version number" do
+    expect(Bank::Data::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
